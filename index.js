2588f3d95c0ebbaa1617b6e2a1be74b5d65be66f
ayaaldabag <ayaaldabag.7@gmail.com>
2025-08-12 11:37:06 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './styles.css';
-import './i18n';
-import LoginPage from './LoginPage';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<LoginPage />);
